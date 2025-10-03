8efd3ec2b0af70f312cb8d9f12302b92f1937086
Matt P <mattp3259@gmail.com>
2025-10-02 23:28:41 -0500

    SaveData2

--- file.go
+++ file.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
 
 func SaveData1(path string, data []byte) error {
 	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
@@ -18,4 +22,34 @@ func SaveData1(path string, data []byte) error {
 	}
 
 	return fp.Sync()
+}
+
+func SaveData2(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
+
+	_, err = fp.Write(data)
+
+	if err != nil {
+		return err
+	}
+
+	err = fp.Sync()
+
+	if err != nil {
+		return err
+	}
+
+	return os.Rename(tmp, path)
 }
